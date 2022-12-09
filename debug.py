3597aadd32d619e942e8b1a5dc1adc542dbecf9b
Daniel Brito <danielbrito@pq.uenf.br>
2022-12-09 10:03:18 -0300

    finally stabilize the swap cost (and loads of other changes :')

--- debug.py
+++ debug.py
@@ -29,24 +29,36 @@ class TSPSolver:
         current_path = current_solution["path"]
         current_eval = current_solution["total_cost"]
         initial_eval = current_eval
+        cooling_rate = 100
+        non_improov = 0
+        max_non_improov = 10
 
+        temperature = 6000
         n = len(current_path)
-        i,j = np.random.choice(range(1,n-1),size=2,replace=False)
-        bounds = (i,j) if i<j else (j,i)
-        swap_cost = self.swap_cost(current_path,bounds)
 
-        if swap_cost < 0:
-            current_path = self.swap_n_reverse(current_path, (i,j))
-            current_eval -= 10 #swap_cost
-            print(self.check_eval(current_eval,current_path))
+        while non_improov < max_non_improov and temperature > 0:
+            i,j = np.random.choice(range(1,n-1),size=2,replace=False)
+            bounds = (i,j) if i<j else (j,i)
+            swap_cost = self.swap_cost(current_path,bounds)
 
-        print(f"initial cost:{initial_eval}; current cost: {current_eval}")
+            if swap_cost < 0:
+                current_path = self.swap_n_reverse(current_path, (i,j))
+                current_eval += swap_cost
+            else:
+                non_improov += 1
+                accepting_prob = np.e**(swap_cost/temperature)
+                if np.random.uniform() < accepting_prob:
+                    current_path = self.swap_n_reverse(current_path, (i,j))
+                    current_eval += swap_cost
+                
+            temperature -= cooling_rate
+
+    
+        print(self.check_eval(current_eval,current_path, swap_cost))
+        print(f"initial cost:{initial_eval}; current cost: {current_eval}; {current_eval/initial_eval}% \n ({i,j})")
 
     def node_distance(self,a,b):
-        try: 
-            distance = self.distance(self.nodes[a], self.nodes[b])
-        except TypeError:
-            print(f"TypeError in node_distance: a,b ={a,b}")
+        distance = self.distance(self.nodes[a], self.nodes[b])
         return distance
 
     def distance(self, a, b):
@@ -57,15 +69,17 @@ class TSPSolver:
 
         
     def swap_n_reverse(self, path, bounds):
+        new_path = np.copy(path)
         i,j = bounds
-        path[i:j] = path[i:j][::-1]
+        new_path[i:j] = new_path[i:j][::-1]
 
-        return path
+        return new_path
 
 
-    def check_eval(self, current_eval,current_path):
+    def check_eval(self, current_eval,current_path, swap_cost):
         calculated_cost = self.cost(current_path)["total_cost"]
-        print(f"eval: {current_eval}; calculated: {calculated_cost}; diff: {calculated_cost-current_eval}")
+        diff = calculated_cost - current_eval + 0.0000001
+        print(f"\n swap/diff: {swap_cost/diff} \n eval: {current_eval}; calculated: {calculated_cost}; diff: {diff} diff% {current_eval/calculated_cost}")
 
         return ((calculated_cost - current_eval)**2)**0.5 < 100
 
@@ -73,16 +87,23 @@ class TSPSolver:
     def swap_cost(self, path, bounds):
         i,j = bounds
         n1,n2 = path[i-1], path[i]
-        n3,n4 = path[j], path[j+1]
+        n3,n4 = path[j-1], path[j]
 
         d = self.node_distance
-        cost = -(d(n1,n2) - d(n1,n4)) + (d(n3,n4) - d(n2,n4))
+        cost = (d(n1,n2) - d(n1,n3)) + (d(n3,n4) - d(n2,n4))
+        test_path = np.copy(path[i-1:j+1])
+        calc_cost_before = self.cost(test_path)["total_cost"]
+        test_path[1:-1] = test_path[1:-1][::-1]
+        calc_cost_after = self.cost(test_path)["total_cost"]
+        calc_cost = calc_cost_before - calc_cost_after
 
-        curr_cost = self.node_distance(path[i-1],path[i]) + self.node_distance(path[j],path[j+1])
-        new_cost = self.node_distance(path[i-1],path[j]) + self.node_distance(path[i],path[j+1])
-        swap_cost = new_cost - curr_cost
-        print(f"old: {swap_cost} cost: {cost}")
-        return cost
+        # curr_cost = d(path[i-1],path[i]) + d(path[j],path[j+1])
+        # new_cost = d(path[i-1],path[j]) + d(path[i],path[j+1])
+        # swap_cost = new_cost - curr_cost
+
+        print(f"swap_cost: {cost}; cost_calc: {calc_cost}; %{cost/calc_cost}")
+
+        return calc_cost
             
   
     def cost(self, path):
